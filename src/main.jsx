844299bf8bf05488b624503b65943a6a8323d76c
udranil Das <youremail@example.com>
2024-12-14 14:25:54 +0530

    Your commit message here

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'; // Add this line to import React
+import React from 'react'; 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-// Create a root and render the App component inside StrictMode
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
